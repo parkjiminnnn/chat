68ea4d69c97aee3ef5d519f4b4034164273e5b2e
parkjiminnnn <pjm5489@naver.com>
2023-09-18 00:53:25 +0900

    완성

--- app.js
+++ app.js
@@ -5,20 +5,29 @@ const path = require("path")
 const server = http.createServer(app);
 
 const socketIO = require("socket.io")
+const moment = require("moment")
+
 
 const io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, "src")))
 
 
-const PORT = process.env.PORT ||5000;
+const PORT = process.env.PORT || 5000;
 
 
-io.on("connection",(socket)=>{
-    socket.on("chatting",(data)=>{
-      console.log(data)
-      io.emit("chatting", '그래 반가워 $(data)')
-    } )
+io.on("connection", (socket) => {
+  socket.on("chatting", (data) => {
+    const {
+      name,
+      msg
+    } = data;
+    io.emit("chatting", {
+      name,
+      msg,
+      time: moment(new Date()).format("h:ss A")
+    })
+  })
 })
 
-server.listen(PORT, () => console.log('server is running ${PORT} '))
+server.listen(PORT, () => console.log(`server is running ${PORT} `))
